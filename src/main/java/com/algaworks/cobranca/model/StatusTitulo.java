6150de022d9902d41acb511aed7fd8c098fa2fd9
Wellington Cruz <wellington.max1@gmail.com>
2019-09-01 22:07:45 -0300

    Adicionando project ao repository.

---
+++ src/main/java/com/algaworks/cobranca/model/StatusTitulo.java
@@ -0,0 +1,16 @@
+package com.algaworks.cobranca.model;
+
+public enum StatusTitulo {
+	PENDENTE("Pendente"),
+	RECEBIDO("Recebido");
+	
+	private String descricao;
+	
+	StatusTitulo(String descricao){
+		this.descricao = descricao;
+	}
+	
+	public String getDescicao() {
+		return this.descricao;
+	}
+}
